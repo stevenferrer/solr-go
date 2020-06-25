3bf2cfca8fb8409ee88a4c9b21ffadef22ca14cc
Steven Ferrer <steven.r.ferrer@gmail.com>
2020-06-25 22:57:26 +0800

    added query and facet methods

--- query/facet.go
+++ query/facet.go
@@ -5,8 +5,26 @@ type Facet struct {
 	Field  string
 	Type   string
 	Facet  map[string]interface{}
-	Domain struct {
-		ExcludeTags string
-		Filters     []string
+	Domain *FDomain
+}
+
+// FDomain is a facet Domain
+type FDomain struct {
+	ExcludeTags string
+	Filters     []string
+}
+
+func NewFacet(field, typeRes string) *Facet {
+	return &Facet{Field: field, Type: typeRes}
+}
+
+func (f *Facet) AddFacet(k, v string) {
+	f.Facet[k] = v
+}
+
+func (f *Facet) SetDomain(excludeTags string, filters []string) {
+	f.Domain = &FDomain{
+		ExcludeTags: excludeTags,
+		Filters:     filters,
 	}
 }
--- query/query.go
+++ query/query.go
@@ -2,28 +2,39 @@ package query
 
 // Query is a query
 type Query struct {
-	facets   map[string]Facet
-	queryStr string
-	queries  map[string]interface{}
-	filters  []string
-	fields   []string
+	facets  map[string]*Facet
+	qs      string
+	queries map[string]interface{}
+	filters []string
+	fields  []string
 }
 
-func NewQuery() *Query {
-	return &Query{}
+// NewQuery is a factory for Query
+func NewQuery(qs string) *Query {
+	return &Query{qs: qs}
 }
 
 // SetQuery sets the main query
-func (q *Query) SetQuery(qs string) {}
+func (q *Query) SetQuery(qs string) {
+	q.qs = qs
+}
 
 // AddQuery adds a query in `queries`
-func (q *Query) AddQuery(k string, v interface{}) {}
+func (q *Query) AddQuery(k string, v interface{}) {
+	q.queries[k] = v
+}
 
 // AddFilter adds a filter
-func (q *Query) AddFilter(filter string) {}
+func (q *Query) AddFilter(filter string) {
+	q.filters = append(q.filters, filter)
+}
 
 // AddField adds a field
-func (q *Query) AddField(field string) {}
+func (q *Query) AddField(field string) {
+	q.fields = append(q.fields, field)
+}
 
 // AddFacet adds a facet
-func (q *Query) AddFacet(facet Facet) {}
+func (q *Query) AddFacet(label string, facet *Facet) {
+	q.facets[label] = facet
+}
