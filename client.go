3b3b10bc367efc9cb60bfa40719b08283e067727
Steven Ferrer <steven.r.ferrer@gmail.com>
2021-01-25 22:24:06 +0800

    implement query method json client

--- client.go
+++ client.go
@@ -1,8 +1,14 @@
 package solr
 
 import (
+	"bytes"
 	"context"
-	"errors"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/pkg/errors"
 )
 
 // Client is an abstraction of a solr client
@@ -12,11 +18,73 @@ type Client interface {
 }
 
 // JSONClient is a solr client that uses the JSON API
-type JSONClient struct{}
+type JSONClient struct {
+	proto      string
+	host       string
+	port       int
+	httpClient *http.Client
+}
 
 var _ Client = (*JSONClient)(nil)
 
+// NewJSONClient returns a new JSONClient
+func NewJSONClient(host string, port int) *JSONClient {
+	return &JSONClient{
+		proto:      "http",
+		host:       host,
+		port:       port,
+		httpClient: http.DefaultClient,
+	}
+}
+
+// WithHTTPClient overrides the default http client
+func (c *JSONClient) WithHTTPClient(httpClient *http.Client) *JSONClient {
+	c.httpClient = httpClient
+	return c
+}
+
 // Query is used for searching/querying
-func (c *JSONClient) Query(ctx context.Context, collection string, qb *QueryBuilder) (*QueryResponse, error) {
-	return nil, errors.New("not implemented")
+func (c *JSONClient) Query(ctx context.Context, collection string,
+	qb *QueryBuilder) (*QueryResponse, error) {
+	theURL, err := url.Parse(fmt.Sprintf("%s://%s:%d/solr/%s/query",
+		c.proto, c.host, c.port, collection))
+	if err != nil {
+		return nil, errors.Wrap(err, "build request url")
+	}
+
+	q, err := qb.Build()
+	if err != nil {
+		return nil, errors.Wrap(err, "build query")
+	}
+
+	var b []byte
+	b, err = json.Marshal(q)
+	if err != nil {
+		return nil, errors.Wrap(err, "marshal query")
+	}
+
+	httpReq, err := http.NewRequestWithContext(ctx,
+		http.MethodPost, theURL.String(), bytes.NewReader(b))
+	if err != nil {
+		return nil, errors.Wrap(err, "new http request")
+	}
+	httpReq.Header.Add("content-type", "application/json")
+
+	var httpResp *http.Response
+	httpResp, err = c.httpClient.Do(httpReq)
+	if err != nil {
+		return nil, errors.Wrap(err, "http do request")
+	}
+
+	var resp QueryResponse
+	err = json.NewDecoder(httpResp.Body).Decode(&resp)
+	if err != nil {
+		return nil, errors.Wrap(err, "decode response")
+	}
+
+	if httpResp.StatusCode > http.StatusOK {
+		return nil, resp.Error
+	}
+
+	return &resp, nil
 }
--- query_response.go
+++ query_response.go
@@ -1,4 +1,16 @@
 package solr
 
 // QueryResponse is a query response
-type QueryResponse struct{}
+type QueryResponse struct {
+	BaseResponse
+	Response QueryResponseBody `json:"response,omitempty"`
+	Facets   M                 `json:"facets,omitempty"`
+}
+
+// QueryResponseBody is the query response body
+type QueryResponseBody struct {
+	NumFound int     `json:"numFound,omitempty"`
+	Start    int     `json:"start,omitempty"`
+	MaxScore float64 `json:"maxScore,omitempty"`
+	Docs     []M     `json:"docs,omitempty"`
+}
